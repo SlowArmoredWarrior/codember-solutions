fdf981b549d8d8a46de1e7c0010719950bee1c96
Slowee <134238128+SlowArmoredWarrior@users.noreply.github.com>
2023-12-24 10:19:06 +0000

    Challenge 02 solved!

--- challenges/02_arithmetic_compiler.js
+++ challenges/02_arithmetic_compiler.js
@@ -1,20 +1,42 @@
-const formula = '&###@&*&###@@##@##&######@@#####@#@#@#@##@@@@@@@@@@@@@@@*&&@@@@@@@@@####@@@@@@@@@#########&#&##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@##@@&';
+import {resolve} from 'path';
+import {readFileSync} from 'fs';
 
+// inside the files folder
+const FILE_PATH = resolve('challenges/files/02.txt');
+
+// content of the file
+const formula = readFileSync(FILE_PATH, {encoding: 'utf8'}).toString();
+
+/**
+ * compiles each expression passed
+ * @param {number} item the accumulator
+ * @return {string | number} number casted as string char or modified number
+ */
 const parseExprItem = {
-  '&': (item) => {
-    console.log(item); return item;
-  },
-
+  '&': (item) => item,
   '#': (item) => item += 1,
   '@': (item) => item -= 1,
   '*': (item) => item *= item,
 };
 
+/**
+ * solves the challenge given the previous functions and vars
+ * @param {string} problem the formula in this case
+ * @return {string}
+ */
 const solution = (problem) => {
   let total = 0;
+  let superStr = '';
+
   for (let formIndex = 0; formIndex < problem.length; formIndex++) {
-    total = parseExprItem[problem[formIndex]](total);
+    // get the compiled expression based of the current symbol
+    const parseStep = parseExprItem[problem[formIndex]](total);
+
+    // prints the value in a string if not modified, otherwise, accums it
+    parseStep === total ? superStr += parseStep : total = parseStep;
   }
+
+  return superStr;
 };
 
-solution(formula);
+console.log(solution(formula));
