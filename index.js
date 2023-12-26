952835871a46d82e061859f584fd0aed7f1ef605
Slowee <134238128+SlowArmoredWarrior@users.noreply.github.com>
2023-12-26 00:50:00 +0000

    feat: Update index.js with new welcome message
    Added new welcome message and instructions on how to run challenge solutions

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello CodeSandbox");
+const message = `\x1b[42m Instructions: \x1b[0m
+    Use the command \x1b[35m node\x1b[0m \x1b[33m challenges/<name_of_challenge>.js\x1b[0m
+    on the terminal to run one of the solutions of the challenges
+
+    \x1b[32m Rules\x1b[0m of the challenge at ./RULES.md
+    \x1b[31m THIS IS AN UNLICENSED REPOSITORY \x1b[0m 
+
+    __Slowee__`;
+
+console.clear();
+console.log("\x1b[45m Welcome to Codember \x1b[0m\n");
+console.log(message);
